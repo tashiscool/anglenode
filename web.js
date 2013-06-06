0b4a37ebc3eff28d7540ed0ed61ccaa305844d9c
ukhant1 <tashdid@gmail.com>
2013-06-06 14:08:04 -0400

    udpating package

--- web.js
+++ web.js
@@ -1,12 +1,19 @@
-var express = require("express");
-var app = express();
-app.use(express.logger());
+var static = require('node-static');
 
-app.get('/', function(request, response) {
-  response.send('Hello World!');
-});
-
+//
+// Create a node-static server instance to serve the './public' folder
+//
+var file = new(static.Server)('./app');
 var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+require('http').createServer(function (request, response) {
+    request.addListener('end', function () {
+        //
+        // Serve files!
+        //
+file.serve(request, response, function(err, result){
+if(err && (err.status === 404)){
+file.serveFile('/index.html', 200, {}, request, response);
+}
+})
+    });
+}).listen(port);
