0a70b04058723aad2b1b0ef98a9f79ab92eeeeee
ukhant1 <tashdid@gmail.com>
2013-06-06 14:40:33 -0400

    removed crap

--- web.js
+++ web.js
@@ -6,14 +6,13 @@ var static = require('node-static');
 var file = new(static.Server)('./app');
 var port = process.env.PORT || 5000;
 require('http').createServer(function (request, response) {
-    request.addListener('end', function () {
+    
         //
         // Serve files!
         //
-file.serve(request, response, function(err, result){
-if(err && (err.status === 404)){
-file.serveFile('/index.html', 200, {}, request, response);
-}
-})
-    });
+        file.serve(request, response, function(err, result){
+            if(err && (err.status === 404)){
+                file.serveFile('/index.html', 200, {}, request, response);
+            }
+        })
 }).listen(port);
